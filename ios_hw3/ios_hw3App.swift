f8cbc8c5c2d330c2d79706c8dd9b811a7f777eed
roadgras <asa881977828@gmail.com>
2022-11-12 17:20:32 -0800

    Initial Commit

---
+++ ios_hw3/ios_hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_hw3App.swift
+//  ios_hw3
+//
+//  Created by li on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct ios_hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
